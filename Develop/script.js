bc80c6200bb0fb36751f1047c49019783c4376d4
xndrbrgs <aborgescolon@gmail.com>
2022-01-06 15:34:29 -0500

    grids done / need to color

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,16 @@ var timeDisplayEl = $('#currentDay');
 // Time displayed on header code
 
 function displayTime() {
-  var rightNow = moment().format("MMMM Do, YYYY");
+  var rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
   timeDisplayEl.text(rightNow);
 }
 
 setInterval(displayTime, 1000);
 
+// Time assigned for each row
 
+function timeNow() {
+    var current = moment().hours();
+    var hoursDay = $('.time-block'); 
+}
 
